ac57a08d76dbf82c9f13e745d161a9a1c503fd61
Michaela-Davis <michaela.delight@gmail.com>
2017-01-20 10:09:47 -0800

    update styling

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// * The program should count up to the provided number
+// * The program should replace numbers divisible by 3 with "ping"
+// * The program should replace numbers divisible by 5 with "pong"
+// * The program should replace numbers divisible by 15 with "pingpong"
+
+// BACK-END / BUSINESS LOGIC
+
+
+// FRONT-END / USER LOGIC
