c8accb1b472d05028ff98a60dcc6791b80b872b9
Michaela-Davis <michaela.delight@gmail.com>
2017-01-20 13:49:38 -0800

    add Business Logic to convert numbers divisible by 3, 5, or 15 to ping, pong, and pingpong respectively

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 // * The program should count up to the provided number
-// * The program should replace numbers divisible by 3 with "ping"
-// * The program should replace numbers divisible by 5 with "pong"
-// * The program should replace numbers divisible by 15 with "pingpong"
 // * The program should allow user to enter a new number and see new results over and over again
 
 // BACK-END / BUSINESS LOGIC
 var convertNumber = function(number) {
-  if (number) {
-    return number;
-  } else {
+  // * The program should replace numbers divisible by 15 with "pingpong"
+  if (number % 15 === 0) {
+    return "pingpong";
+  // * The program should replace numbers divisible by 5 with "pong"
+  } else if (number % 5 === 0) {
+    return "pong";
+  // * The program should replace numbers divisible by 3 with "ping"
+  } else if (number % 3 === 0) {
+    return "ping";
+  } else if (!number) {
     alert("Enter a number");
+  } else {
+    return number;
   }
 };
 
