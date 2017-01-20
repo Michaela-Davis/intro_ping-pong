6cbe16dec53f392a076c7fe1fd74890cdb013651
Michaela-Davis <michaela.delight@gmail.com>
2017-01-20 12:42:25 -0800

    add user logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,21 @@
 // * The program should allow user to enter a new number and see new results over and over again
 
 // BACK-END / BUSINESS LOGIC
-
+var convertNumber = function(number) {
+  if (number) {
+    console.log(number)
+  } else {
+    alert("Enter a number")
+  }
+};
 
 // FRONT-END / USER LOGIC
 $(document).ready(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
 
+    var number = parseInt($("#userInput").val());
+    var result = convertNumber(number);
+    $("#result").text(result);
   });
 });
