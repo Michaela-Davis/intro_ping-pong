ea334cea20d081a30e67ed5a91aec4b5c0541945
Michaela-Davis <michaela.delight@gmail.com>
2017-01-29 21:36:23 -0800

    move comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-// * The program should count up to the provided number
-// * The program should allow user to enter a new number and see new results over and over again
-
 // BACK-END / BUSINESS LOGIC
 
+// * The program should count up to the provided number
 var createPingPong = function(number) {
   var resultArray = [];
 
@@ -34,6 +32,8 @@ var convertNumber = function(number) {
 
 // FRONT-END / USER LOGIC
 $(document).ready(function() {
+
+  // * The program should allow user to enter a new number and see new results over and over again
   $(".form").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#userInput").val());
